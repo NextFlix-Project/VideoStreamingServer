8f6890b90e14408965ca515160e66676aaaed1fe
Christopher Riffle <cmr629@gmail.com>
2023-09-09 23:51:25 -0400

    fixing issues

---
+++ VideoStreamingServer/VideoStreamingServer/CMakeFiles/libdash_external.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libdash_external"
+  "CMakeFiles/libdash_external-complete"
+  "libdash/src/libdash_external-stamp/libdash_external-build"
+  "libdash/src/libdash_external-stamp/libdash_external-configure"
+  "libdash/src/libdash_external-stamp/libdash_external-download"
+  "libdash/src/libdash_external-stamp/libdash_external-install"
+  "libdash/src/libdash_external-stamp/libdash_external-mkdir"
+  "libdash/src/libdash_external-stamp/libdash_external-patch"
+  "libdash/src/libdash_external-stamp/libdash_external-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/libdash_external.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
